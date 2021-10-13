597ecfa7ac2b0b469733694eab9b92b8f6b14e2e
Lukas Winkler <git@lw1.at>
2021-10-13 17:51:58 +0200

    fix gunicorn

--- gunicorn.py
+++ gunicorn.py
@@ -1,5 +1,5 @@
-pidfile = "/srv/server/live/runtime/pid"
-bind = "unix:/srv/server/live/runtime/socket"
+pidfile = "/srv/live/runtime/pid"
+bind = "unix:/srv/live/runtime/socket"
 proc_name = "matomocamp-live"
 worker_tmp_dir = "/dev/shm"
 workers = 3
